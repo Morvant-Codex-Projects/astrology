72a231f0a0cf2692886a7c8d95ca27e3c549e5e5
Danielle-design-art <daniellemorvant1@gmail.com>
2025-05-06 09:51:41 -0500

    trying to make button display sign accordingly

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 console.log("Hello")
 
+
+if (Date >= 03/21 && Date <= 04/19) {
+    console.log ("Aries");
+} else if (Date >= 4/20 && Date <= 05/20) {
+    console.log("Taurus");
+} else if (Date >= 5/21 && Date <= 6/20) {
+    console.log("Gemini");
+} else if (Date >= 6/21 && Date <= 07/22) {
+    console.log("Cancer");
+} else if (Date >= 07/23 && Date <= 08/22) {
+    console.log("Leo"); 
+} else if (Date >= 08/23 && Date <= 09/22) {
+    console.log("Virgo");
+} else if (Date >= 09/23 && Date <= 10/22) {
+    console.log("Libra");
+} else if (Date >= 10/23 && Date <= 11/21) {
+    console.log("Scorpio");
+} else if (Date >= 11/22 && Date <= 12/21) {
+    console.log("Sagittarius");
+}  else if (Date >= 12/22 && Date <= 01/19) {
+    console.log("Capricorn");
+}  else if (Date >= 01/20 && Date <= 02/18) {
+    console.log("Aquarius") 
+}  else if (Date >= 01/20 && Date <= 02/18) {
+    console.log("Pisces")
+}
+
+
